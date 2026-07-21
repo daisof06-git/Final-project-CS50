36024e5729ae1e886a608abded26048e884f5e26
daisof06 <daisof06@gmail.com>
2026-07-21 00:22:34 -0300

    database creation

--- create_db.py
+++ create_db.py
@@ -1,4 +1,10 @@
 import sqlite3
 connection = sqlite3.connect("budget.db")
+with open("schema.sql") as file:
+    schema = file.read()
+
+connection.executescript(schema)
 
 connection.close()
+
+print("Base initialized!")
---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username TEXT NOT NULL, 
+    hash TEXT NOT NULL, 
+    balance NUMERIC,
+    email TEXT UNIQUE NOT NULL);
+CREATE UNIQUE INDEX username ON users (username);
+
+CREATE TABLE jars(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id INTEGER NOT NULL,
+    jar_name TEXT NOT NULL,
+    income BOOLEAN DEFAULT FALSE, 
+    amount INTEGER, 
+    FOREIGN KEY(user_id) REFERENCES users(id));
+
+CREATE TABLE movements(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
+    new_jar_id INTEGER NOT NULL, 
+    last_jar_id INTEGER NOT NULL,
+    date DATE NOT NULL DEFAULT CURRENT_DATE,
+    FOREIGN KEY(user_id) REFERENCES users(id), 
+    FOREIGN KEY(new_jar_id) REFERENCES jars(id), 
+    FOREIGN KEY(last_jar_id) REFERENCES jars(id)); 
