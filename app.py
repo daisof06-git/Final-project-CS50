a9fa9c1b09c6caa084f40e7390aa4b9212c60057
daisof06 <daisof06@gmail.com>
2026-07-21 22:31:56 -0300

    app structure

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 import sqlite3
-
+from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required, usd
 
 # Configure application
 app = Flask(__name__)
@@ -12,4 +13,50 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+@login_required
+def index():
+    pass
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    #log user in
+    pass
+
+@app.route("/income", methods = ["GET", "POST"])
+@login_required
+def add_income():
+    pass 
+
+@app.route("/jars", methods = ["GET", "POST"])
+@login_required
+def jars():
+    pass
+
+@app.route("/logout", methods = ["GET", "POST"])
+@login_required
+def logout():
+    pass
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    pass
+
+@app.route("/budget", methods = ["GET", "POST"])
+@login_required
+def budget():
+    pass
+
 
