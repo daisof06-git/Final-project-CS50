c169450013bf15d522281a37792c092a5f785e16
daisof06 <daisof06@gmail.com>
2026-07-21 22:07:12 -0300

    app configuration

---
+++ app.py
@@ -0,0 +1,7 @@
+import os
+import sqlite3
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
