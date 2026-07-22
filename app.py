c13b8f5ba25576a332d3966f1be3c4d1d8e34555
daisof06 <daisof06@gmail.com>
2026-07-21 22:56:14 -0300

    app structure

--- app.py
+++ app.py
@@ -33,7 +33,39 @@ def index():
 @app.route("/login", methods = ["GET", "POST"])
 def login():
     #log user in
-    pass
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            pass
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            pass
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            pass
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 @app.route("/income", methods = ["GET", "POST"])
 @login_required
